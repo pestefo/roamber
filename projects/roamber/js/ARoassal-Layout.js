a86bbfd0da95ca18f14aadb8509949f8c5e9f96c
Pablo Estefo <pestefo@gmail.com>
2013-07-22 18:11:06 -0400

    Refactor: Layout stuff now is in ARoassal-Layout

---
+++ projects/roamber/js/ARoassal-Layout.js
@@ -0,0 +1,389 @@
+smalltalk.addPackage('ARoassal-Layout');
+smalltalk.addClass('ROLayout', smalltalk.Object, ['translator'], 'ARoassal-Layout');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "applyOn:",
+category: 'not yet classified',
+fn: function (elements) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { _st(self)._executeOnElements_(elements);
+return self}, function($ctx1) {$ctx1.fill(self,"applyOn:",{elements:elements},smalltalk.ROLayout)});},
+args: ["elements"],
+source: "applyOn: elements\x0a\x09\x0a\x09self executeOnElements: elements",
+messageSends: ["executeOnElements:"],
+referencedClasses: []
+}),
+smalltalk.ROLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "executeOnElements:",
+category: 'not yet classified',
+fn: function (elements) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { _st(self)._doExecute_(_st(elements)._asOrderedCollection());
+return self}, function($ctx1) {$ctx1.fill(self,"executeOnElements:",{elements:elements},smalltalk.ROLayout)});},
+args: ["elements"],
+source: "executeOnElements: elements \x0a\x09\x22Execute the layout, myself, on the elements\x22\x0a\x09\x09\x0a\x22\x09self doInitialize: elements.\x22\x0a\x09self doExecute: elements asOrderedCollection.\x0a\x22\x09self doPost: elements.\x22",
+messageSends: ["doExecute:", "asOrderedCollection"],
+referencedClasses: []
+}),
+smalltalk.ROLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "initialize",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { self["@translator"]=_st((smalltalk.ROLayoutTranslator || ROLayoutTranslator))._default();
+return self}, function($ctx1) {$ctx1.fill(self,"initialize",{},smalltalk.ROLayout)});},
+args: [],
+source: "initialize\x0a\x09translator := ROLayoutTranslator default.",
+messageSends: ["default"],
+referencedClasses: ["ROLayoutTranslator"]
+}),
+smalltalk.ROLayout);
+
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "on:",
+category: 'not yet classified',
+fn: function (aCollection) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+_st(_st(self)._new())._applyOn_(aCollection);
+$1=aCollection;
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"on:",{aCollection:aCollection},smalltalk.ROLayout.klass)});},
+args: ["aCollection"],
+source: "on: aCollection\x0a\x09self new applyOn: aCollection.\x0a\x09^ aCollection",
+messageSends: ["applyOn:", "new"],
+referencedClasses: []
+}),
+smalltalk.ROLayout.klass);
+
+
+smalltalk.addClass('ROAbstractLineLayout', smalltalk.ROLayout, ['gapSize', 'horizontalGap', 'verticalGap', 'horizontalOutGap', 'verticalOutGap', 'alignment', 'horizontallyStretchable', 'verticallyStretchable'], 'ARoassal-Layout');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "alignTop",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { self["@alignment"]=smalltalk.symbolFor("top");
+return self}, function($ctx1) {$ctx1.fill(self,"alignTop",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "alignTop\x0a\x09\x0a\x09alignment := #top",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "alignment",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=self["@alignment"];
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"alignment",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "alignment\x0a\x09\x0a\x09^alignment",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "alignment:",
+category: 'not yet classified',
+fn: function (anObject) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { self["@alignment"]=anObject;
+return self}, function($ctx1) {$ctx1.fill(self,"alignment:",{anObject:anObject},smalltalk.ROAbstractLineLayout)});},
+args: ["anObject"],
+source: "alignment: anObject\x0a\x09\x0a\x09alignment := anObject",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "horizontalGap",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=self["@horizontalGap"];
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"horizontalGap",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "horizontalGap\x0a\x09\x0a\x09^horizontalGap",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "horizontalOutGap",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=self["@horizontalOutGap"];
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"horizontalOutGap",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "horizontalOutGap\x0a\x09\x0a\x09^horizontalOutGap",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "initialize",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { smalltalk.ROLayout.fn.prototype._initialize.apply(_st(self), []);
+self["@verticalGap"]=(10);
+self["@horizontalGap"]=(10);
+self["@horizontalOutGap"]=(5);
+self["@verticalOutGap"]=(5);
+self["@verticallyStretchable"]=false;
+self["@horizontallyStretchable"]=self["@verticallyStretchable"];
+return self}, function($ctx1) {$ctx1.fill(self,"initialize",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "initialize\x0a\x09\x0a\x09super initialize.\x0a\x09verticalGap := 10.\x0a\x09horizontalGap := 10.\x0a\x09horizontalOutGap := 5.\x0a\x09verticalOutGap := 5.\x0a\x09horizontallyStretchable := verticallyStretchable := false",
+messageSends: ["initialize"],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "verticalOutGap",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=self["@verticalOutGap"];
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"verticalOutGap",{},smalltalk.ROAbstractLineLayout)});},
+args: [],
+source: "verticalOutGap\x0a\x09\x0a\x09^verticalOutGap",
+messageSends: [],
+referencedClasses: []
+}),
+smalltalk.ROAbstractLineLayout);
+
+
+
+smalltalk.addClass('ROHorizontalLineLayout', smalltalk.ROAbstractLineLayout, [], 'ARoassal-Layout');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "deltaFor:",
+category: 'not yet classified',
+fn: function (aNodeFigure) {
+var self=this;
+var delta;
+return smalltalk.withContext(function($ctx1) { var $1,$2,$3;
+delta=(0);
+$1=_st(_st(self)._alignment()).__eq_eq(smalltalk.symbolFor("bottom"));
+if(smalltalk.assert($1)){
+delta=_st(aNodeFigure)._height();
+delta;
+};
+$2=_st(_st(self)._alignment()).__eq_eq(smalltalk.symbolFor("center"));
+if(smalltalk.assert($2)){
+delta=_st(_st(aNodeFigure)._height()).__slash((2));
+delta;
+};
+$3=_st((0)).__at(delta);
+return $3;
+}, function($ctx1) {$ctx1.fill(self,"deltaFor:",{aNodeFigure:aNodeFigure,delta:delta},smalltalk.ROHorizontalLineLayout)});},
+args: ["aNodeFigure"],
+source: "deltaFor: aNodeFigure\x0a\x09\x0a\x09| delta |\x0a\x09delta := 0.\x0a\x09self alignment == #bottom ifTrue: [delta := aNodeFigure height].\x0a\x09self alignment == #center ifTrue: [delta := aNodeFigure height / 2.0].\x0a\x09^0 @ delta",
+messageSends: ["ifTrue:", "height", "==", "alignment", "/", "@"],
+referencedClasses: []
+}),
+smalltalk.ROHorizontalLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "doExecute:",
+category: 'not yet classified',
+fn: function (elements) {
+var self=this;
+var pointer,delta;
+return smalltalk.withContext(function($ctx1) { pointer=_st(self)._positionOriginalPointer_(elements);
+_st(elements)._do_((function(element){
+return smalltalk.withContext(function($ctx2) {delta=_st(self)._deltaFor_(element);
+delta;
+_st(self["@translator"])._translate_to_(element,_st(pointer).__minus(delta));
+pointer=_st(self)._movePointer_accordingToFigure_(pointer,element);
+return pointer;
+}, function($ctx2) {$ctx2.fillBlock({element:element},$ctx1)})}));
+return self}, function($ctx1) {$ctx1.fill(self,"doExecute:",{elements:elements,pointer:pointer,delta:delta},smalltalk.ROHorizontalLineLayout)});},
+args: ["elements"],
+source: "doExecute: elements \x0a\x09| pointer delta |\x0a\x09pointer := self positionOriginalPointer: elements.\x0a\x22\x0a\x09horizontallyStretchable ifTrue: \x0a\x09\x09[ self doStretchHorizontal: elements ].\x0a\x22\x0a\x09elements do: \x0a\x09\x09[ :element | \x0a\x09\x09delta := self deltaFor: element.\x0a\x09\x09translator translate: element to: pointer - delta.\x0a\x09\x09pointer := self \x0a\x09\x09\x09\x09\x09movePointer: pointer\x0a\x09\x09\x09\x09\x09accordingToFigure: element.\x0a ].\x09\x0a\x22\x0a\x09verticallyStretchable ifTrue: \x0a\x09\x09[ self doStretchVertical: elements ]\x0a\x22",
+messageSends: ["positionOriginalPointer:", "do:", "deltaFor:", "translate:to:", "-", "movePointer:accordingToFigure:"],
+referencedClasses: []
+}),
+smalltalk.ROHorizontalLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "initialize",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { smalltalk.ROAbstractLineLayout.fn.prototype._initialize.apply(_st(self), []);
+_st(self)._alignTop();
+return self}, function($ctx1) {$ctx1.fill(self,"initialize",{},smalltalk.ROHorizontalLineLayout)});},
+args: [],
+source: "initialize\x0a\x09\x0a\x09super initialize.\x0a\x09self alignTop",
+messageSends: ["initialize", "alignTop"],
+referencedClasses: []
+}),
+smalltalk.ROHorizontalLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "movePointer:accordingToFigure:",
+category: 'not yet classified',
+fn: function (pointer, element) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=_st(_st(_st(_st(pointer)._x()).__plus(_st(element)._width())).__plus(_st(self)._horizontalGap())).__at(_st(pointer)._y());
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"movePointer:accordingToFigure:",{pointer:pointer,element:element},smalltalk.ROHorizontalLineLayout)});},
+args: ["pointer", "element"],
+source: "movePointer: pointer accordingToFigure: element\x0a\x09\x0a\x09^ (pointer x + element width + self horizontalGap) @ pointer y",
+messageSends: ["@", "y", "+", "horizontalGap", "width", "x"],
+referencedClasses: []
+}),
+smalltalk.ROHorizontalLineLayout);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "positionOriginalPointer:",
+category: 'not yet classified',
+fn: function (aGraph) {
+var self=this;
+var maxHeight,delta;
+return smalltalk.withContext(function($ctx1) { var $1,$2,$3;
+delta=(0);
+$1=_st(_st(self)._alignment()).__eq_eq(smalltalk.symbolFor("bottom"));
+if(smalltalk.assert($1)){
+maxHeight=_st(_st(aGraph)._nodes())._maxValue_(smalltalk.symbolFor("height"));
+maxHeight;
+delta=maxHeight;
+delta;
+};
+$2=_st(_st(self)._alignment()).__eq_eq(smalltalk.symbolFor("center"));
+if(smalltalk.assert($2)){
+maxHeight=_st(_st(aGraph)._nodes())._maxValue_(smalltalk.symbolFor("height"));
+maxHeight;
+delta=_st(maxHeight).__slash((2));
+delta;
+};
+$3=_st(_st(self)._horizontalOutGap()).__at(_st(_st(self)._verticalOutGap()).__plus(delta));
+return $3;
+}, function($ctx1) {$ctx1.fill(self,"positionOriginalPointer:",{aGraph:aGraph,maxHeight:maxHeight,delta:delta},smalltalk.ROHorizontalLineLayout)});},
+args: ["aGraph"],
+source: "positionOriginalPointer: aGraph \x0a\x09| maxHeight delta |\x0a\x09delta := 0.\x0a\x09self alignment == #bottom ifTrue: \x0a\x09\x09[ maxHeight := aGraph nodes maxValue: #height.\x0a\x09\x09delta := maxHeight ].\x0a\x09self alignment == #center ifTrue: \x0a\x09\x09[ maxHeight := aGraph nodes maxValue: #height.\x0a\x09\x09delta := maxHeight / 2.0 ].\x0a\x09^ self horizontalOutGap @ (self verticalOutGap + delta)",
+messageSends: ["ifTrue:", "maxValue:", "nodes", "==", "alignment", "/", "@", "+", "verticalOutGap", "horizontalOutGap"],
+referencedClasses: []
+}),
+smalltalk.ROHorizontalLineLayout);
+
+
+
+smalltalk.addClass('ROLayoutTranslator', smalltalk.Object, [], 'ARoassal-Layout');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "translate:to:",
+category: 'not yet classified',
+fn: function (element, newPosition) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { _st(self)._subclassResponsibility();
+return self}, function($ctx1) {$ctx1.fill(self,"translate:to:",{element:element,newPosition:newPosition},smalltalk.ROLayoutTranslator)});},
+args: ["element", "newPosition"],
+source: "translate: element to: newPosition\x0a\x09self subclassResponsibility",
+messageSends: ["subclassResponsibility"],
+referencedClasses: []
+}),
+smalltalk.ROLayoutTranslator);
+
+
+smalltalk.ROLayoutTranslator.klass.iVarNames = ['defaultTranslator'];
+smalltalk.addMethod(
+smalltalk.method({
+selector: "default",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1,$2,$3;
+$1=self["@defaultTranslator"];
+if(($receiver = $1) == nil || $receiver == undefined){
+$1;
+} else {
+$2=self["@defaultTranslator"];
+return $2;
+};
+self["@defaultTranslator"]=_st(_st(self)._defaultClass())._new();
+$3=self["@defaultTranslator"];
+return $3;
+}, function($ctx1) {$ctx1.fill(self,"default",{},smalltalk.ROLayoutTranslator.klass)});},
+args: [],
+source: "default\x0a\x09\x22Return the default translator, the one used in the layouts\x22\x0a\x0a\x09defaultTranslator ifNotNil: [ ^ defaultTranslator ].\x0a\x09^ defaultTranslator := self defaultClass new",
+messageSends: ["ifNotNil:", "new", "defaultClass"],
+referencedClasses: []
+}),
+smalltalk.ROLayoutTranslator.klass);
+
+smalltalk.addMethod(
+smalltalk.method({
+selector: "defaultClass",
+category: 'not yet classified',
+fn: function () {
+var self=this;
+return smalltalk.withContext(function($ctx1) { var $1;
+$1=(smalltalk.RODirectLayoutTranslator || RODirectLayoutTranslator);
+return $1;
+}, function($ctx1) {$ctx1.fill(self,"defaultClass",{},smalltalk.ROLayoutTranslator.klass)});},
+args: [],
+source: "defaultClass\x0a\x09^ RODirectLayoutTranslator",
+messageSends: [],
+referencedClasses: ["RODirectLayoutTranslator"]
+}),
+smalltalk.ROLayoutTranslator.klass);
+
+
+smalltalk.addClass('RODirectLayoutTranslator', smalltalk.ROLayoutTranslator, [], 'ARoassal-Layout');
+smalltalk.addMethod(
+smalltalk.method({
+selector: "translate:to:",
+category: 'not yet classified',
+fn: function (element, newPosition) {
+var self=this;
+return smalltalk.withContext(function($ctx1) { _st(element)._translateTo_(newPosition);
+return self}, function($ctx1) {$ctx1.fill(self,"translate:to:",{element:element,newPosition:newPosition},smalltalk.RODirectLayoutTranslator)});},
+args: ["element", "newPosition"],
+source: "translate: element to: newPosition\x0a\x09element translateTo: newPosition",
+messageSends: ["translateTo:"],
+referencedClasses: []
+}),
+smalltalk.RODirectLayoutTranslator);
+
+
+
