d0785a05f4a32d349da6dff7d6a37f6302bc58e5
Pablo Estefó <pestefo@gmail.com>
2014-01-17 11:18:56 -0300

    Deleted files

--- projects/roamber/js/ARoassal-Interaction-Test.js
+++
@@ -1,7 +0,0 @@
-define("roamber/ARoassal-Interaction-Test", ["amber_vm/smalltalk", "amber_vm/nil", "amber_vm/_st", "roamber/ARoassal-Test"], function(smalltalk,nil,_st){
-smalltalk.addPackage('ARoassal-Interaction-Test');
-smalltalk.packages["ARoassal-Interaction-Test"].transport = {"type":"amd","amdNamespace":"roamber"};
-
-smalltalk.addClass('ROPopupTest', smalltalk.ROTest, ['e', 'view'], 'ARoassal-Interaction-Test');
-
-});
